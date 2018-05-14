820fe36a77c2199b5744c70ef4a25377b9f12d5b
Valehelle <hazmiirfan92@gmail.com>
2018-05-14 22:21:52 +0800

    add carousell

--- app/containers/alphabet/view.js
+++ app/containers/alphabet/view.js
@@ -3,12 +3,59 @@ import ReactNative from 'react-native'
 import { connect } from 'react-redux'
 import styles from './style'
 import  Button  from '../../components/button/view'
+import Carousel from 'react-native-snap-carousel';
+
 
 const {
     View,
     Text,
     Image,
+    Dimensions,
 } = ReactNative
+
+export const ENTRIES1 = [
+    {
+        title: 'Beautiful and dramatic Antelope Canyon',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://i.imgur.com/UYiroysl.jpg'
+    },
+    {
+        title: 'Earlier this morning, NYC',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/UPrs1EWl.jpg'
+    },
+    {
+        title: 'White Pocket Sunset',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+        illustration: 'https://i.imgur.com/MABUbpDl.jpg'
+    },
+    {
+        title: 'Acrocorinth, Greece',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://i.imgur.com/KZsmUi2l.jpg'
+    },
+    {
+        title: 'The lone tree, majestic landscape of New Zealand',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/2nCt3Sbl.jpg'
+    },
+    {
+        title: 'Middle Earth, Germany',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/lceHsT6l.jpg'
+    }
+];
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+function wp (percentage) {
+    const value = (percentage * viewportWidth) / 100;
+    return Math.round(value);
+}
+const itemHorizontalMargin = wp(2);
+const slideWidth = wp(75);
+export const sliderWidth = viewportWidth;
+export const itemWidth = slideWidth + itemHorizontalMargin * 2;
+
 
 class Alphabet extends Component{
 
@@ -40,6 +87,14 @@ class Alphabet extends Component{
     }
 
 
+    _renderItem ({item, index}) {
+        return (
+            <View style={{backgroundColor: 'blue'}}>
+                <Text style={styles.title}>{ item.title }</Text>
+            </View>
+        );
+    }
+
     _renderDinosour(){
         if(this.props.dinosours != undefined ){
             const nextPosition = this.state.position + 1
@@ -50,6 +105,14 @@ class Alphabet extends Component{
                      />
                     <Text>Name: {this.props.dinosours[this.state.position].name} </Text>
                     <Button states={{title: this.props.dinosours[nextPosition].alphabet.toString() ,onPress: this.nextButtonPress.bind(this)}} />
+                    <Carousel
+                        layout={'default'} 
+                        ref={(c) => { this._carousel = c; }}
+                        data={ENTRIES1}
+                        renderItem={this._renderItem}
+                        sliderWidth={sliderWidth}
+                        itemWidth={itemWidth}
+                    />
                 </View>
            )
         }
